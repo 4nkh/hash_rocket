8d7fa5e14d0895a0ccd9de5d0f764e1d4fe702c8
sparkplug <mathieub@4nkh.com>
2012-08-31 07:47:05 -0400

    patch 0.0.4

---
+++ lib/hash_rocket/tasks.rb
@@ -0,0 +1,6 @@
+namespace 'hash_rocket' do
+  desc 'find and replace old hash_rockets'
+  task convert: :environment do 
+    HasHRocket.convert
+  end
+end
--- lib/tasks.rb
+++
@@ -1,6 +0,0 @@
-namespace "hash_rocket" do
-  desc "find and replace old hash_rockets"
-  task convert: :environment do 
-    HasHRocket.convert
-  end
-end

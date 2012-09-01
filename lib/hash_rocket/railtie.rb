2932e216c91f887ed5796b23875ba8fa0d4a13cb
Mbougie <bbob784@gmail.com>
2012-08-31 20:37:01 -0400

    patch 0.0.9

--- lib/hash_rocket/railtie.rb
+++ lib/hash_rocket/railtie.rb
@@ -1,7 +1,12 @@
 require 'hash_rocket'
-require 'rails'
-class Railtie < Rails::Railtie
-  rake_tasks do
-    require 'lib/hash_rocket/tasks.rb'
+
+module HashRocket 
+  if defined? Rails::Railtie
+    require 'rails'
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        require 'lib/hash_rocket/tasks.rb'
+      end
+    end
   end
 end

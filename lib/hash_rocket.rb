4d87afd74a6420b0469c530dc2d5999303e22b8d
root <root@sparkplug-linux.(none)>
2012-09-02 23:17:17 -0400

    patch 0.1.6

--- lib/hash_rocket.rb
+++ lib/hash_rocket.rb
@@ -1,51 +1,42 @@
 require "hash_rocket/version"
-require "iconv"
 module HashRocket
   
-  @file_names = Dir["#{Rails.root}/**/*"]
-
+  if defined?(Rails)
+    require "hash_rocket/railtie"
+    @file_names = Dir.glob("**/*") 
+  end
+  
   def self.convert
     @file_names.each do |fn|
       if fn =~ /\.(rb|erb|haml|spec)/
         begin
-          text = File.read(fn)
-          text = solve_invalid_bit_sequence_in_utf8(text)
+          text = File.read(Rails.root.to_s + "/" + fn)
           text = organize_symbols(text)
           File.open(fn, "w") {|file| file.puts text }
-        rescue
-          puts "ERROR ON #{fn}"
+        rescue 
+          puts "ERROR ON #{fn}" 
         end
       end
     end
   end
-
 private
-  # TODO use String#encode instead of Iconv
-  def solve_invalid_bit_sequence_in_utf8(text)
-    ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-    return ic.iconv(text)
-  end
-
   def self.organize_symbols(text)
-    without_space = text.scan(/:[a-z]{1,}\ =>/).flatten
-    text = match_symbols(text, without_space, ": ") if without_space
-
-    with_space = text.scan(/:[a-z]{1,}\ =>\ /).flatte
+    with_space = text.scan(/\ :\w{1,}\ =>\ /).flatten
     text = match_symbols(text, with_space, ":") if with_space
+
+    without_space = text.scan(/\ :\w{1,}\ =>/).flatten
+    text = match_symbols(text, without_space, ": ") if without_space
     text
   end
   
   def self.match_symbols(text, strings, comma)
     strings.uniq.each do |str|
-      comma, symbol, hash_rocket = str.match(/(^:*)(.+)(\ =>)/i).captures
-      text = text.gsub(str, symbol + comma)
+      comma, symbol, hash_rocket = str.match(/(^\ :*)(.+)(\ =>)/i).captures
+      symbol_wrap = symbol + comma
+      text = text.gsub(str, " " + symbol_wrap.gsub(/\ /, ""))
     end
-    text
+    text 
   end
 
 private_class_method :organize_symbols, :match_symbols
-end
-
-if defined?(Rails)
-  require "hash_rocket/railtie"
 end

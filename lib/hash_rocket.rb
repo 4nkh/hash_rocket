f7c4a30b4ea130ddea19ab98a6cb4274ae44ad97
sparkplug <mathieub@4nkh.com>
2012-09-04 01:59:16 -0400

    patch 0.2.0
    fix FOLDER option

--- lib/hash_rocket.rb
+++ lib/hash_rocket.rb
@@ -8,30 +8,35 @@ module HashRocket
   end
   
   def self.convert(folder=nil, path=nil)
-    file_names = path ? [%W(#{path})] : Dir.glob("**/*")
+    file_names = path_parameters(folder, path)
     file_names.each do |fn|
       if fn =~ /\.(rb|erb|haml|html|spec)/
-        begin
+        #begin
           text = retrieve_file(folder, path, fn)
           text = solve_invalid_bit_sequence_in_utf8(text)
           text = organize_symbols(text)
           File.open(fn, "w") {|file| file.puts text }
-        rescue 
-          puts "ERROR ON #{fn}" 
-        end
+        #rescue 
+        #  puts "ERROR ON #{fn}" 
+        #end
       end
     end
   end
 private
+  def self.path_parameters(folder,path)
+    return path ? %W(#{path}) : Dir.glob("**/*") unless folder
+    return Dir[folder + "/**/*"] if folder
+  end
+
   def self.retrieve_file(folder, path, fn)
-    File.read((folder ? folder.gsub(/\/$/,'') : Rails.root.to_s) + "/" + fn) unless path
-    File.read(path) if path
+    return File.read(folder ? fn : Rails.root.to_s + "/" + fn) unless path
+    return File.read(path) if path
   end
 
   # TODO use String#encode instead of Iconv
   def self.solve_invalid_bit_sequence_in_utf8(text)
     ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-    ic.iconv(text)
+    return ic.iconv(text)
   end
 
   def self.organize_symbols(text)
@@ -50,5 +55,5 @@ private
     text 
   end
 
-private_class_method :retrieve_file, :organize_symbols, :match_symbols, :solve_invalid_bit_sequence_in_utf8
+private_class_method :path_parameters, :retrieve_file, :organize_symbols, :match_symbols, :solve_invalid_bit_sequence_in_utf8
 end
